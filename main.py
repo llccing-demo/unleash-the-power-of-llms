4b1d09753b5847531dd7e24788657d1dbe98954d
porter2021 <lcf33123@gmail.com>
2024-07-11 10:37:31 +0800

    feat: langchain chat

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
 # Set your OpenAI API key here
 api_key = os.getenv("OPENAI_API_KEY")
 url = "https://oneapi.gptnb.me/v1"
-llm = OpenAI(
+model = ChatOpenAI(
+    model="gpt-4o",
     openai_api_key=api_key,
     openai_api_base=url
 )
 
-response = llm.invoke("what is the tallest building in the world?")
+messages = [
+    SystemMessage(content="Translate the following from English into Chinese"),
+    HumanMessage(content="Hi !")
+]
+
+response = model.invoke(messages)
 print(response)
 
