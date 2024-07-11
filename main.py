856cd81b3e448294f92e8f35a5077eca1aac79d3
porter2021 <lcf33123@gmail.com>
2024-07-11 14:55:24 +0800

    feat: Add Pydantic (JSON) parser

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+from typing import List
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
-from langchain.output_parsers import DatetimeOutputParser, CommaSeparatedListOutputParser
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate, ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field, validator
+from langchain.output_parsers import DatetimeOutputParser, CommaSeparatedListOutputParser, PydanticOutputParser
 
 load_dotenv()
 
@@ -17,6 +19,50 @@ model = ChatOpenAI(
     temperature=0.0
 )
 
+# defining the author class for the model
+class Author(BaseModel):
+    number: int = Field(description="number of books written by the author")
+    books: List[str] = Field(description="list of books they wrote")
+
+user_query = "Generate the books written by 鲁迅"
+
+# defining the output parser
+output_parser = PydanticOutputParser(pydantic_object=Author)
+
+# defining the prompt template with parser instructions
+prompt1 = PromptTemplate(
+    template="Answer the user query. \n{format_instructions}\n{query}\n",
+    input_variables=['query'],
+    partial_variables={"format_instructions": output_parser.get_format_instructions()},
+)
+
+# this is the latest version usecase in langchain doc
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user query. Wrap the output in `json` tags\n{format_instructions}",
+        ),
+        ("human", "{query}")
+    ]
+).partial(format_instructions=output_parser.get_format_instructions())
+
+# defining the prompt
+my_prompt = prompt1.format_prompt(query=user_query)
+
+# converting the output to a string while generating the response
+output = model.invoke(my_prompt.to_string())
+
+print(output)
+
+# Clean the output by removing code block delimiters
+#cleaned_output = output.content.strip().strip('```json').strip('```')
+
+# printing the result
+print(output_parser.parse(output.content))
+
+
+# Datetime and List parser
 parser_dateTime = DatetimeOutputParser()
 parser_List = CommaSeparatedListOutputParser()
 
